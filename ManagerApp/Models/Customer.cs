e938fd331b17ec15230eef96d99c1887feb8a942
szemand <szeman.david@gmail.com>
2021-12-10 03:01:42 +0100

    Added form validation

--- ManagerApp/Models/Customer.cs
+++ ManagerApp/Models/Customer.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ManagerApp.Models
 {
     public class Customer
     {
         public long Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"[\w ]*[^\W_][\w ]*")]
         public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"[\w ]*[^\W_][\w ]*")]
         public string CarType { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]{3}-\d{3}$")]
         public string LicensePlate { get; set; }
+
+        [Required]
         public string Description { get; set; }
         public string Status { get; set; }
         public DateTime DateAdded { get; set; }
