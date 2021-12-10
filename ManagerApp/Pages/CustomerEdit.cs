ce523f295a0c15a890624cc4cba26a8d113b0194
szemand <szeman.david@gmail.com>
2021-12-10 02:25:05 +0100

    Added component to edit customer details

---
+++ ManagerApp/Pages/CustomerEdit.cs
@@ -0,0 +1,37 @@
+﻿using ManagerApp.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ManagerApp.Pages
+{
+    public partial class CustomerEdit
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public Customer Customer { get; set; }
+
+        [Parameter]
+        public string CustomerID { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Customer = await HttpClient.GetFromJsonAsync<Customer>($"customer/{CustomerID}");
+            await base.OnInitializedAsync();
+        }
+
+        private async Task SubmitForm()
+        {
+            await HttpClient.PutAsJsonAsync("customer", Customer);
+            NavigationManager.NavigateTo("customers");
+        }
+    }
+}
