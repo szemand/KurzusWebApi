fb2d375a48758376469f1c7d5ba2b11a53fe632c
szemand <szeman.david@gmail.com>
2021-12-10 02:01:47 +0100

    Added component to view customer details

---
+++ ManagerApp/Pages/CustomerDetails.cs
@@ -0,0 +1,28 @@
+﻿using ManagerApp.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ManagerApp.Pages
+{
+    public partial class CustomerDetails
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        [Parameter]
+        public string CustomerID { get; set; }
+
+        public Customer Customer { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Customer = await HttpClient.GetFromJsonAsync<Customer>($"customer/{CustomerID}");
+            await base.OnInitializedAsync();
+        }
+    }
+}
