cbcf8ca7a51117728dabc7ad3d6ad9d0f58cf7c6
szemand <szeman.david@gmail.com>
2021-12-10 04:04:20 +0100

    Added car mechanic's client project with task listing

---
+++ MechanicApp/Pages/TaskList.cs
@@ -0,0 +1,25 @@
+﻿using ManagerApp.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace MechanicApp.Pages
+{
+    public partial class TaskList
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        public Customer[] Customers { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Customers = await HttpClient.GetFromJsonAsync<Customer[]>("customer");
+            await base.OnInitializedAsync();
+        }
+    }
+}
